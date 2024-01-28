e782481e5d8515f115c910ecfc7ee2d219f29f4b
naveen-developer <51435497+naveen-developer@users.noreply.github.com>
2024-01-28 20:02:00 +0530

    second commit

--- index.js
+++ index.js
@@ -23,26 +23,120 @@ const quizData = {
     ],
     quiz1: [
         {
-            question: "question1",
-            ans: "question1ans"
+            question: "She ___ a dog",
+            ans: "She has a dog"
         },
         {
-            question: "question2",
-            ans: "question2ans"
+            question: "Does she ___ a son",
+            ans: "Does she have a son"
         },
         {
-            question: "How do you write 'Hello World' in an alert box?",
-            ans: "alert('Hello World');"
+            question: "Does they ___ a business in Dubai",
+            ans: "Does they have a business in Dubai"
         },
         {
-            question: "How do you create a function in JavaScript?",
-            ans: "function myFunction()"
+            question: "It ___ 3 tyres",
+            ans: "It has 3 tyres"
         },
         {
-            question: "How do you call a function named 'myFunction'?",
-            ans: "myFunction()"
+            question: "My car ___ a full tank of petrol",
+            ans: "My car has a full tank of petrol"
         },
-    ]
+        {
+            question: "She ___ a headache",
+            ans: "She has a headache"
+        },
+        {
+            question: "Shinto doesn’t ___ long hair",
+            ans: "Shinto doesn’t have long hair"
+        },
+        {
+            question: "How many sisters does he ___ ",
+            ans: "How many sisters does he have"
+        },
+        {
+            question: "How many legs does a spider 🕷 ___ ",
+            ans: "How many legs does a spider 🕷have"
+        },
+        {
+            question: "Doctors ___ a separate canteen ",
+            ans: "Doctors have a separate canteen"
+        },
+    ],
+    quiz2: [
+        {
+            question: "అతనికి పిల్లలు లేరు",
+            ans: "They don't have children"
+        },
+        {
+            question: "దీనికి ప్రత్యేక ఎంపిక ఉంది",
+            ans: "It has special feature"
+        },
+        {
+            question: "జేమ్స్‌కు ఎస్‌.బి.ఐ బ్యాంక్‌లో ఖాతా ఉంది",
+            ans: "James has account in SBI bank."
+        },
+        {
+            question: "అతనికి ఎంతమంది సోదరులు ఉన్నారు?",
+            ans: "How many friends does he have ?"
+        },
+        {
+            question: "వైజాగ్లో వారికి అపార్ట్‌మెంట్ ఉందా?",
+            ans: "Does they have appartment in vizag ?"
+        },
+    ],
+    quiz3: [
+        {
+            question: "నిన్న ఒక Wedding function ఉన్నింది. నేను వెళ్ళాను.కాని నేను ఏ ఆహారం కూడా తినలేదు. నేను అనారోగ్యంతో ఉన్నాను.",
+            ans: "There was a wedding function yesterday. I went but I didn't have food last night because I am sick."
+        },
+        {
+            question: "నిన్న నాకు సినిమాకి వెళ్ళే ప్లాన్ ఉన్నింది. నా దగ్గర డబ్బులు లేవు. నేను మా అమ్మని డబ్బులు అడిగాను. అమ్మ దగ్గర డబ్బులు లేవు.",
+            ans: "I had a plan to go to cinema yesterday. I don't have money. I asked my mother for money. She doesn't have money."
+        },
+    ],
+    quiz4: [
+        {
+            question: "మీరు ఆహారం తిన్నారా?",
+            ans: "Did you have food ?"
+        },
+        {
+            question: "అతను lunch చేశాడా?  (భోజనం)",
+            ans: "Did he have lunch"
+        },
+        {
+            question: "రాజు భోజనం తినలేదు",
+            ans: "Raju didn't have food"
+        },
+        {
+            question: "ఈ రోజు మీకు duty ఉన్నిందా",
+            ans: "Did you have duty today?"
+        },
+        {
+            question: " నాకు ఏ ఆశ లేదు",
+            ans: "I don't have any hope"
+        },
+        {
+            question: "ఈ రోజు నాకు meeting లేదు",
+            ans: "I don't have meeting today"
+        },
+        {
+            question: "ఈ రోజు ఆమెకు class లేదు",
+            ans: "she doesn't have class today"
+        },
+        {
+            question: "నా దగ్గర డబ్బు లేదు",
+            ans: "I don't have money"
+        },
+        {
+            question: "నేను ఉదయం చిప్స్ తిన్నాను",
+            ans: "I ate  chips in the morning"
+        },
+        {
+            question: "నేను నిన్న రాత్రి ఆహారం తినలేదు",
+            ans: "I didn't eat food last night"
+        },
+    ],
 };
 
 //console.log(quizData);
@@ -88,8 +182,6 @@ QuizData.forEach((val, outerIndex) => {
         console.log('answer' + outerIndex);
         val[1].forEach((question, innerIndex) => {
             let inputValue = document.getElementById(`answer${outerIndex}${innerIndex}`);
-            //console.log("Input ", inputValue);
-            //console.log("Input Value ", inputValue.value);
             if ((question.ans).trim().toUpperCase() === (inputValue.value).trim().toUpperCase()) {
                 inputValue.style.backgroundColor = "#ade5af";
             } else {
@@ -97,7 +189,6 @@ QuizData.forEach((val, outerIndex) => {
                     inputValue.style.backgroundColor = "#ffa29b";
                     document.querySelector(`.correct_ans_style${outerIndex}${innerIndex}`).style.display = "block";
                     document.getElementById(`correct_ans${outerIndex}${innerIndex}`).innerHTML = question.ans;
-                    //console.log(question.ans)
                 }
             }
         });
